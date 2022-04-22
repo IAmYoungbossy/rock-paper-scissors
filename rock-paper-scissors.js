523e62559bc91bfa71403c145ef2932fdc7a1875
Letam Bossman Barinua <letambarinua@gmail.com>
2022-04-22 21:50:00 +0100

    Wrong if..else result
    After creating a function called computerPlay that returned
    random Rock, Paper, Scissors, I find it hard using if..else
    statement to return a statement deending on 6 of the likely
    outcomes.
    
    I'm pausing here for the day, I'll be doing more reserch on
    if...else satements, switch and functions to know how i can
    get my way through.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,40 @@
 function computerPlay() {
     let a = Math.random();
     let b = Math.floor(a*3)+1;
+if (b === 1) {
+    b = 'rock';  
+} else if (b === 2) {
+    b = 'paper'
+} else if (b === 3) {
+    b = 'scissors'}
     return b;
 }
 
-console.log(computerPlay());
+const player = 'ROCK';/*prompt('Enter either Rock, Paper or Scissors to play');*/
+const play = player.toLowerCase();
+
+if (computerPlay() == 'rock') {
+    if (play == 'paper') {
+        console.log('You Win! Paper beats Rock.');
+    }
+} if (computerPlay() == 'paper') {
+    if (play == 'rock') {
+        console.log('You Lose! Paper beats Rock.');
+    }
+} if (computerPlay() == 'paper') {
+    if (play == 'scissors') {
+        console.log('You Win! Scissors cuts Paper.');
+    }
+} if (computerPlay() == 'scissors') {
+    if (play == 'paper') {
+        console.log('You Lose! Scissors cuts Paper.');
+    }
+} if (computerPlay() == 'rock') {
+    if (play == 'scissors') {
+        console.log('You Lose! Rock crushes Scissors.');
+    }
+} if (computerPlay() == 'scissors') {
+    if (play == 'rock') {
+        console.log('You Win! Rock crushes Scissors.');
+    }
+}
