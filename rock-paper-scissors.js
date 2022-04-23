23c9b972d1290a00327a4c588296a1e4f3a11d9c
Letam Bossman Barinua <letambarinua@gmail.com>
2022-04-23 23:02:52 +0100

    Create function playRound
    This function gives six different outcome that can result from
    playing Rock Paper Scissors and what to do under such conditions.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,18 +7,31 @@ function getRandomNum() {
     return randomInt;
 }
 
-let randomNum = getRandomNum();
+let randomNum = getRandomNum();//Save function to variable.
 
-//Assign each random numbers to Rock, Paper and Scissors
+//Assign each random numbers to Rock, Paper and Scissors.
 function randomNumToWords() {
     if (randomNum === 1) {
-        return 'rock';
+        return 'ROCK';
     } else if (randomNum === 2) {
-        return 'paper';
+        return 'PAPER';
     } else if (randomNum === 3) {
-        return 'scissors';
+        return 'SCISSORS';
     } else {
         alert('Error: Something wrong with game algorithm')
     }
 }
-console.log(randomNumToWords());
+
+let computerPlay = randomNumToWords()//Save function to variable.
+
+//Function for one round play.
+function playRound(playerSelection, computerSelection) {
+    let message = (playerSelection === 'ROCK' && computerSelection === 'PAPER')? 'You Lose! Paper beats Rock.':
+    (playerSelection === 'PAPER' && computerSelection === 'SCISSORS')? 'You Lose! Scissors cuts Paper.':
+    (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')? 'You Lose! Rock crushes Scissors.':
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')? 'You Win! Paper beats Rock.':
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')? 'You Win! Scissors cuts Paper.':
+    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS')? 'You Win! Rock crushes Scissors.':
+    (playerSelection === computerSelection)? 'It\'s a Tie.': 'Invalid Play!.'
+    return message;
+}
