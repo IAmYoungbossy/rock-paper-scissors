fd424f8e50eb3a0a46004866176434078adb0f99
Letam Bossman Barinua <letambarinua@gmail.com>
2022-04-23 21:52:00 +0100

    Convert random numbers to Rock, Paper, Scissors
    Assign each of the random numbers generated to either Rock,
    Scissors or Paper.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,21 @@
 //Rock Paper Scissors Project From The Odin Project
 
+//Function to generate random numbers between 1 - 3
 function getRandomNum() {
     const randomFloat = Math.random();
     const randomInt = Math.floor(randomFloat*3)+1;
     return randomInt;
 }
 
-console.log(getRandomNum());
+//Assign each random numbers to Rock, Paper and Scissors
+function randomNumToWords() {
+    if (getRandomNum === 1) {
+        return 'rock';
+    } else if (getRandomNum === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+console.log(randomNumToWords());
